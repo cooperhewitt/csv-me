42c520d9e14951670c5bfdf27b788293370f1d41
Micah Walter <micah@micahwalter.com>
2014-03-13 12:32:48 -0400

    setting up basic home page stuff

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import request
+
+import cooperhewitt.api.client
+
+import json
+import csv
+
+access_token = os.environ['CH_API_KEY']
+hostname = os.environ['CH_API_HOST']
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    api = cooperhewitt.api.client.OAuth2(access_token, hostname=hostname)
+    method = 'cooperhewitt.exhibitions.getList'
+      
+    rsp = api.call(method) 
+    exhibitions = rsp['exhibitions']
+    
+    return render_template('index.html', exhibitions=exhibitions)
