9eeeb2c75811c28014941ff4b16f2d4be7bce26f
Micah Walter <micah@micahwalter.com>
2014-03-13 13:46:03 -0400

    fixed redis url

--- run-worker.py
+++ run-worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL')
 if not redis_url:
     raise RuntimeError('Set up Redis To Go first.')
 
